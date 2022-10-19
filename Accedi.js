a18b62ce9b57d3cd7d60706d9eac5821611f7e13
Aliexpress2401 <alicecrudele2401@gmail.com>
2022-10-19 15:01:09 +0200

    html

---
+++ Accedi.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    alert("BENVENUTO! \nAccesso avvenuto con successo!");
+  }
---
+++ Iscriviti.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    alert("BENVENUTO! \nIscrizione avvenuta con successo!");
+  }
